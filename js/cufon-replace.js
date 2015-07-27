a2b07794e96edf946b37302162fa259a83d13c11
orielz <orielux@gmail.com>
2015-07-27 12:31:24 +0300

    Added site dir to root

--- js/cufon-replace.js
+++
@@ -1,3 +0,0 @@
-Cufon.replace('.logo', { fontFamily: 'Lobster', hover:true });
-Cufon.replace('.menu li a.item', { fontFamily: 'PT Sans Narrow', hover:true });
-Cufon.replace('.menu li a.item-1', { fontFamily: 'PT Sans Narrow', hover:true });
